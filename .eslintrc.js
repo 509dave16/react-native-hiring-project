db4bd5fbbc8b04acd60fe1cc5cb09ff002a60827
David Fall <david@madebycodera.com>
2020-09-14 12:45:08 -0700

    RNHP - Make semicolons not required by eslint or prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   root: true,
+  rules: {
+    semi: ['off', 'never'],
+  },
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
