60b990b3a3cc04397cb71170aa978b0225858602
David Fall <david@madebycodera.com>
2020-09-14 12:46:31 -0700

    RNHP - Initial barebones stub of main screen where inspections list will be shown

---
+++ components/features/Inspection/List.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import {View, Text} from 'react-native'
+
+function List() {
+  return (
+    <View>
+      <Text>Inspections List</Text>
+    </View>
+  )
+}
+
+export default List
